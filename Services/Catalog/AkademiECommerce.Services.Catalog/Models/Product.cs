e83a1caf370644fb4b09dae43e829e49e1bf366b
acarnilsu <nilsuacar42@gmail.com>
2023-01-11 21:08:45 +0300

    Add project files.

---
+++ Services/Catalog/AkademiECommerce.Services.Catalog/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AkademiECommerce.Services.Catalog.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public int Stock { get; set; }
+    }
+}
