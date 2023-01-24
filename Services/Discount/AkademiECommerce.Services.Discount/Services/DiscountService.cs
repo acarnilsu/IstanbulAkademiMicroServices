d2dd23c1f4194bff702e94ccb31e7ae523588de8
acarnilsu <nilsuacar42@gmail.com>
2023-01-24 21:09:32 +0300

    Discount Finished

--- Services/Discount/AkademiECommerce.Services.Discount/Services/DiscountService.cs
+++ Services/Discount/AkademiECommerce.Services.Discount/Services/DiscountService.cs
@@ -1,40 +1,86 @@
 ﻿using AkademiECommerce.Shared.Dtos;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AkademiECommerce.Services.Discount.Services
 {
     public class DiscountService : IDiscountService
     {
+        private readonly IConfiguration _configuration;
+        private readonly IDbConnection _dbconnection;
+
+
+
+        public DiscountService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _dbconnection = new NpgsqlConnection(_configuration.GetConnectionString("PostgreSql"));
+        }
+
+
 
         public async Task<ResponseDto<NoContent>> Delete(int id)
         {
-            throw new System.NotImplementedException();
+            var status = await _dbconnection.ExecuteAsync("Delete from Discount where id=@Id", new { Id = id });
+            return status > 0 ? ResponseDto<NoContent>.Success(204) : ResponseDto<NoContent>.Fail("Silinecek değer bulunamadı", 404);
         }
+
+
 
         public async Task<ResponseDto<List<Models.Discount>>> GetAll()
         {
-            throw new System.NotImplementedException();
+            var discount = await _dbconnection.QueryAsync<Models.Discount>("Select * from discount");
+            return ResponseDto<List<Models.Discount>>.Success(discount.ToList(), 200);
+
+
+
         }
+
+
 
         public async Task<ResponseDto<Models.Discount>> GetByCodeUser(string code, string id)
         {
             throw new System.NotImplementedException();
         }
 
+
+
         public async Task<ResponseDto<Models.Discount>> GetById(int id)
         {
-            throw new System.NotImplementedException();
+            var discount = (await _dbconnection.QueryAsync<Models.Discount>("Select * from discount where id=@Id", new { Id = id })).SingleOrDefault();
+            if (discount == null)
+            {
+                return ResponseDto<Models.Discount>.Fail("Değer bulunamadı", 404);
+            }
+            return ResponseDto<Models.Discount>.Success(discount, 200);
         }
+
+
 
         public async Task<ResponseDto<NoContent>> Insert(Models.Discount discount)
         {
-            throw new System.NotImplementedException();
+            var status = await _dbconnection.ExecuteAsync("Insert into discount (UserId,Rate,Code) values (@UserId, @Rate, @Code)", discount);
+            return status > 0 ? ResponseDto<NoContent>.Success(204) : ResponseDto<NoContent>.Fail("Bir hata oluştu", 500);
         }
+
+
 
         public async Task<ResponseDto<NoContent>> Update(Models.Discount discount)
         {
-            throw new System.NotImplementedException();
+            var status = await _dbconnection.ExecuteAsync("Update Discount set userid=@UserId, rate=@Rate, code=@Code where id=@Id",
+            new
+            {
+                Id = discount.ID,
+                UserId = discount.UserID,
+                Rate = discount.Rate,
+                Code = discount.Code
+            });
+            return status > 0 ? ResponseDto<NoContent>.Success(204) : ResponseDto<NoContent>.Fail("Bir hata oluştu", 500);
         }
     }
 }
--- Services/Discount/AkademiECommerce.Services.Discount/Startup.cs
+++ Services/Discount/AkademiECommerce.Services.Discount/Startup.cs
@@ -1,6 +1,11 @@
+using AkademiECommerce.Services.Discount.Services;
+using AkademiECommerce.Shared.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,7 +31,22 @@ namespace AkademiECommerce.Services.Discount
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+            services.AddHttpContextAccessor();
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+            services.AddScoped<IDiscountService, DiscountService>();
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(ops =>
+            {
+                ops.Authority = Configuration["IdentityServerURL"];
+                ops.Audience = "resource_Discount";
+                ops.RequireHttpsMetadata = false;
+            });
+            services.AddControllers(opts =>
+            {
+                opts.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -44,7 +65,7 @@ namespace AkademiECommerce.Services.Discount
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
